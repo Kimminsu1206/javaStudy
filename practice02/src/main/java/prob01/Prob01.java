d2b3147ef38107a8c284792a0e4a19c9906b4f69
Kimminsu <rlaalstn1055@naver.com>
2021-09-07 19:06:52 +0900

    hi

---
+++ practice02/src/main/java/prob01/Prob01.java
@@ -0,0 +1,23 @@
+package prob01;
+
+import java.util.Scanner;
+
+public class Prob01 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner( System.in  );
+
+		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
+
+		int money;
+		
+		
+		System.out.print("금액을 입력하세요. : ");
+		money = scanner.nextInt();
+		
+		for(int i=0; i<10; i++) {
+			money / MONEYS[i]
+		}
+		
+		scanner.close();
+ 	}
+}
