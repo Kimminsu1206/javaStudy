e78a936e6443fab5bd36581eacdcd5e54ff43063
Kimminsu <rlaalstn1055@naver.com>
2021-09-07 20:28:07 +0900

    home

--- practice02/src/main/java/prob01/Prob01.java
+++ practice02/src/main/java/prob01/Prob01.java
@@ -4,21 +4,20 @@ import java.util.Scanner;
 
 public class Prob01 {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner( System.in  );
+		Scanner scanner = new Scanner(System.in);
 
 		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
 
 		int money;
-		
-		
+
 		System.out.print("금액을 입력하세요. : ");
 		money = scanner.nextInt();
-		
+
 		for (int i = 0; i < MONEYS.length; i++) {
-	        System.out.println(MONEYS[i]+"원: "+ money / MONEYS[i] + "개");
-	        money%= MONEYS[i];
-		
- 	}
+			System.out.println(MONEYS[i] + "원: " + money / MONEYS[i] + "개");
+			money %= MONEYS[i];
+
+		}
 		scanner.close();
-}
+	}
 }
--- practice02/src/main/java/prob02/Prob02.java
+++ practice02/src/main/java/prob02/Prob02.java
@@ -7,21 +7,20 @@ public class Prob02 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		int[] intArray = new int[ 5 ];
+		int[] intArray = new int[5];
 		double sum = 0;
-		
+
 		System.out.println("5개의 숫자를 입력하세요 : ");
-		
-		for(int i=0; i<intArray.length; i++) {
-		int num = scanner.nextInt();
-		
-		sum += num;
-		
+
+		for (int i = 0; i < intArray.length; i++) {
+			int num = scanner.nextInt();
+
+			sum += num;
+
 		}
-		
+
 		System.out.print("평균은 " + sum / 5.0 + " 입니다.");
-		
+
 		scanner.close();
 	}
 }
-
