0368a29089f9cd4607ab3c3813e6e98f0af9f5c6
Kimminsu <rlaalstn1055@naver.com>
2021-09-07 20:23:41 +0900

    dong-hyeon

---
+++ Practice01/src/main/java/Practice01/Prob4.java
@@ -0,0 +1,20 @@
+package Practice01;
+
+import java.util.Scanner;
+
+public class Prob4 {
+
+	public static void main(String[] args) {
+		System.out.print("문자열을 입력하세요 : ");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		
+		for(int i = 0; i<=input.length(); i++) {
+			System.out.println(input.substring(0, i));
+		}
+		
+		sc.close();
+
+	}
+
+}
