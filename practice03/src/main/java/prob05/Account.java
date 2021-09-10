59ed476ab68e5d058533cebfc0878b9d6439beaa
Kimminsu <rlaalstn1055@naver.com>
2021-09-10 20:31:25 +0900

    09/10

--- practice03/src/main/java/prob05/Account.java
+++ practice03/src/main/java/prob05/Account.java
@@ -1,4 +1,40 @@
 package prob05;
 
 public class Account {
-}
+	
+		private String accountNo;
+		private int balance;
+		
+		public Account(String no) {
+			this.accountNo = no;
+			System.out.println(accountNo + " 계좌가 개설되었습니다.");
+		}
+		
+		void save(int value) {
+			this.balance += value;
+			System.out.println(accountNo + " 계좌에 " + value + "만원이 입금되었습니다.");
+		}
+		
+		void deposit(int value) {
+			this.balance -= value;
+			System.out.println(accountNo + " 계좌에 " + value + "만원이 출금되었습니다.");
+		}
+
+		public String getAccountNo() {
+			return accountNo;
+		}
+
+		public void setAccountNo(String accountNo) {
+			this.accountNo = accountNo;
+		}
+
+		public int getBalance() {
+			return balance;
+		}
+
+		public void setBalance(int balance) {
+			this.balance = balance;
+		}
+
+		
+	}
